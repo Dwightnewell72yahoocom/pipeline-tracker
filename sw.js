41a92e0d356a24ddf594da62233e95acf09f929a
Dwightnewell72yahoocom <dwightnewell72@yahoo.com>
2026-05-21 18:53:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,24 @@
-const CACHE = 'parsons-pipeline-v5';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
+const CACHE = 'parsons-pipeline-v6';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) { return cache.addAll(ASSETS); })
-  );
+  e.waitUntil(caches.open(CACHE).then(function(cache) { return cache.addAll(ASSETS); }));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k){ return k !== CACHE; })
-            .map(function(k){ return caches.delete(k); })
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(cached) {
-      // Always try network first, fall back to cache
-      return fetch(e.request).then(function(response) {
-        if (!response || response.status !== 200) return cached || response;
-        var clone = response.clone();
-        caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
-        return response;
-      }).catch(function(){
-        return cached || caches.match('./index.html');
-      });
+    fetch(e.request).then(function(response) {
+      if (!response || response.status !== 200) return response;
+      var clone = response.clone();
+      caches.open(CACHE).then(function(cache){ cache.put(e.request, clone); });
+      return response;
+    }).catch(function(){
+      return caches.match(e.request).then(function(cached){ return cached || caches.match('./index.html'); });
     })
   );
 });
